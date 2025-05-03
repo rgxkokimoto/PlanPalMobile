04b87a1a146787276fe1e8e865b48c853dd9ba06
GitWithAlex <109067549+rgxkokimoto@users.noreply.github.com>
2025-05-03 23:38:51 +0200

    cambio de nomenglatura

--- app/src/main/java/com/example/planpalmobile/ui/calendar/ItemEventFragment.java
+++ app/src/main/java/com/example/planpalmobile/ui/calendar/ItemEventFragment.java
@@ -12,10 +12,10 @@ import com.example.planpalmobile.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EventDetailFragment#newInstance} factory method to
+ * Use the {@link ItemEventFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EventDetailFragment extends Fragment {
+public class ItemEventFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class EventDetailFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public EventDetailFragment() {
+    public ItemEventFragment() {
         // Required empty public constructor
     }
 
@@ -39,8 +39,8 @@ public class EventDetailFragment extends Fragment {
      * @return A new instance of fragment EventDetailFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EventDetailFragment newInstance(String param1, String param2) {
-        EventDetailFragment fragment = new EventDetailFragment();
+    public static ItemEventFragment newInstance(String param1, String param2) {
+        ItemEventFragment fragment = new ItemEventFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class EventDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_detail, container, false);
+        return inflater.inflate(R.layout.fragment_event_item, container, false);
     }
 }
